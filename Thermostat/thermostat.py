9c931498c39ac3e4c80eb4322db2efa394c56e79
ddanaei <ddanaei@lely.com>
2022-07-12 12:39:40 +0200

    add set_point to thermostat

--- Thermostat/thermostat.py
+++ Thermostat/thermostat.py
@@ -2,10 +2,12 @@ class Thermostat:
     def __init__(self, hour: int = None, temperature: int = None) -> None:
         self.hour = None
         self.set_hour(hour)
+        self.lock_temperature = False
         self.temperature = None
         self.set_temperature(temperature)
 
     def set_hour(self, hour: int) -> None:
+        self.lock_temperature = False
         if 0 <= hour <= 24:
             self.hour = hour
         else:
@@ -13,7 +15,10 @@ class Thermostat:
             self.hour = 0
 
     def set_temperature(self, temperature: int) -> None:
-        if 15 <= temperature <= 30:
+
+        if self.lock_temperature:
+            print("Temperature is locked to {}".format(self.get_temperature()))
+        elif 15 <= temperature <= 30:
             self.temperature = temperature
         else:
             print("Temperature can only be between 15 and 30. Temperature set to 15.")
@@ -25,9 +30,7 @@ class Thermostat:
     def get_temperature(self) -> int:
         return self.temperature
 
-    # def get_current_required_temperature(self) -> None:
-    #     pass
-
-    # def add_set_point(self, hour: int, temperature: int):
-
-
+    def add_set_point(self, start_hour: int, stop_hour: int, temperature: int) -> None:
+        if start_hour <= self.hour <= stop_hour:
+            self.set_temperature(temperature)
+            self.lock_temperature = True
--- Thermostat/thermostat_test.py
+++ Thermostat/thermostat_test.py
@@ -2,21 +2,24 @@ import unittest
 
 from Thermostat.thermostat import Thermostat
 
-thermostat = Thermostat(14, 20)
-
 
 class ThermostatTest(unittest.TestCase):
 
     def test_get_hour(self):
-        # arrange, act and assert
+        # arrange
+        thermostat = Thermostat(14, 20)
+        # act and assert
         self.assertEqual(thermostat.get_hour(), 14)
 
     def test_get_temperature(self):
-        # arrange, act and assert
+        # arrange
+        thermostat = Thermostat(14, 20)
+        # act and assert
         self.assertEqual(thermostat.get_temperature(), 20)
 
     def test_set_hour(self):
         # arrange
+        thermostat = Thermostat(14, 20)
         thermostat.set_hour(15)
         # act and assert
         self.assertEqual(thermostat.get_hour(), 15)
@@ -27,6 +30,7 @@ class ThermostatTest(unittest.TestCase):
 
     def test_set_temperature(self):
         # arrange
+        thermostat = Thermostat(14, 20)
         thermostat.set_temperature(15)
         # act and assert
         self.assertEqual(thermostat.get_temperature(), 15)
@@ -37,11 +41,17 @@ class ThermostatTest(unittest.TestCase):
 
     # def test_get_current_required_temperature(self):
 
-    # def test_set_point(self):
-    #     # arrange
-    #     thermostat = Thermostat()
-    #     # act and assert
-    #     self.assertEqual(thermostat.add_set_point(hour=15, temperature=18), (15, 18))
+    def test_set_point(self):
+        # arrange
+        thermostat = Thermostat(14, 20)
+        thermostat.set_hour(17)
+        thermostat.add_set_point(start_hour=15, stop_hour=20, temperature=21)
+        # act and assert
+        self.assertEqual(thermostat.get_temperature(), 21)
+        # arrange
+        thermostat.set_temperature(18)
+        # act and assert
+        self.assertEqual(thermostat.get_temperature(), 21)
 
 
 if __name__ == '__main__':
